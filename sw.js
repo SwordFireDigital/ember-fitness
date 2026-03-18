60b2fd4199d206a5946b00909720838831f8929c
Sword Fire Digital <209007305+SwordFireDigital@users.noreply.github.com>
2026-03-18 14:42:05 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'ember-fitness-v1';
+const PRECACHE = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
